9c199783665f18a1a25a03bd5819f463144786f9
OlegRoma <mr.grinch1910@gmail.com>
2022-08-17 12:05:09 +0300

    создали UseCase

---
+++ app/src/main/java/com/example/domain/AddShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.domain
+
+//добавить элемент в список
+class AddShopItemUseCase {
+    fun addShopItem(shopItem: ShopItem){
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.domain
+
+//удалить элемент из списка
+class DeleteShopItemUseCase {
+    fun deleteShopItem(shopItem: ShopItem){
+        TODO()
+    }
+}
